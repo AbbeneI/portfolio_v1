5b6a2b2939948c2ba3ba048fa0eb6f6be094d3ae
iabbene <ianabbene@gmail.com>
2023-07-03 14:48:35 -0500

    Set up repo

---
+++ src/utils/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+
+    email: 'ianabbene@gmail.com',
+
+    socialMedia: [
+        {
+            name: 'GitHub',
+            url: '',
+        },
+        {
+            name: 'Instagram',
+            url: '',
+        },
+        {
+            name: 'Twitter',
+            url: '',
+        },
+        {
+            name: 'Linkedin',
+            url: '',
+        },
+        {
+            name: 'Codepen',
+            url: '',
+        },
+    ],
+
+    navLinks: [
+        {
+            name: 'About',
+            url: '/#about',
+        },
+        {
+            name: 'Experience',
+            url: '/#experience',
+        },
+        {
+            name: 'Work',
+            url: '/#work',
+        },
+        {
+            name: 'Contact',
+            url: '/#contact',
+        },
+    ],
+
+    colors: {
+    }
+}
