4c5767ce8e8b3f4fee9e5d8fbba4672129ed53c2
iabbene <ianabbene@gmail.com>
2023-07-03 16:17:24 -0500

    Update favicon, title

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+// import { Hero } from '../../components';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+
+      </header>
+
+      <main className="App">
+        {/* <Hero /> */}
+        {/* <About />
+        <Jobs />
+        <Featured />
+        <Projects />
+        <Contact /> */}
+      </main>
+    </>
+  );
+}
+
+export default App;
